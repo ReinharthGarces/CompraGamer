7023b8a0ae47f7fe0921b369dfd2cce22c7fa518
Reinharth Garces <rjgarces7@gmail.com>
2023-05-11 15:46:33 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './product/product.model'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular-CompraGamer';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'CompraGamer';
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>("https://static.compragamer.com/test/productos.json")
+    .subscribe((data) => {
+      this.products = data;
+    });
+  }
+}
